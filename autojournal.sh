c824fc0566e70644a50858562452cc1b471229de
Aaron Thompson <aaroncthompson1@gmail.com>
2023-03-07 02:38:52 +0000

    Added inline input or prompt

--- autojournal.sh
+++ autojournal.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/bash
 
-read -p "Type your journal entry: " entry
+# Check if user put input inline, otherwise prompt them
+if [[ -z "$1" ]]; then
+	read -p "Type your journal entry: " entry
+else
+	entry="$1"
+fi
+
+# Concatenate the date timestamp, a delimter, and input
 dt=$(TZ=America/Chicago date +%Y-%m-%d-%T) 
 padding=" - "
 line="$dt$padding$entry"
+
+# Log the concatenated input to a file
 #echo $line
 echo $line >> ~/journal.md 
