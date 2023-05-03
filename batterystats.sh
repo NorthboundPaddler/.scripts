4238df9726f52ac103a66b8d6a7f85ea5ba6a866
Aaron Thompson <aaroncthompson1@gmail.com>
2023-05-02 20:44:35 -0500

    removed print of color

--- batterystats.sh
+++ batterystats.sh
@@ -11,13 +11,10 @@ CAP="$(cat /sys/class/power_supply/$BATTERY/capacity)"
 STATE="$(cat /sys/class/power_supply/$BATTERY/status)"
 
 if [[ $CAP<30 ]]; then
-	echo "red"
 	COLOR="$RED"
 elif [[ $CAP<80 ]]; then
-	echo "yellow"
 	COLOR="$YELLOW"
 else
-	echo "green"
 	COLOR="$GRN"
 fi
 
