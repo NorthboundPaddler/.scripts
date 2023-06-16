357bfb4a2604c742e90d2176cd6926b7361be05a
Aaron Thompson <aaroncthompson1@example.com>
2023-06-16 06:40:38 -0500

    Added +/- prefix and better format

--- chargingspeed.sh
+++ chargingspeed.sh
@@ -1,1 +1,16 @@
-awk '{print $1*10^-6 " W"}' /sys/class/power_supply/BAT1/power_now
+#!/usr/bin/bash
+#
+state="`cat /sys/class/power_supply/BAT1/status`"
+echo "$state"
+if [ "$state"="Discharging" ]; then
+	prefix="-"
+else
+	prefix="+"
+fi
+
+uw="`cat /sys/class/power_supply/BAT1/power_now`"
+w=$((uw/1000000))
+
+echo "$prefix$w Watts"
+
+# awk '{print $1*10^-6 " W"}' /sys/class/power_supply/BAT1/power_now
