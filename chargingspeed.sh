52cd3e768b4f497d315d2b775a8e252f6edd5aec
Aaron Thompson <aaroncthompson1@example.com>
2023-07-22 20:38:03 -0500

    tweaks to batt

--- chargingspeed.sh
+++ chargingspeed.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/bash
-#
-state="`cat /sys/class/power_supply/BAT1/status`"
-echo "$state"
-if [ "$state"="Discharging" ]; then
-	prefix="-"
-else
-	prefix="+"
-fi
 
+state="`cat /sys/class/power_supply/BAT1/status`"
 uw="`cat /sys/class/power_supply/BAT1/power_now`"
 w=$((uw/1000000))
 
-echo "$prefix$w Watts"
+echo "$state @ $w Watts"
 
 # awk '{print $1*10^-6 " W"}' /sys/class/power_supply/BAT1/power_now
